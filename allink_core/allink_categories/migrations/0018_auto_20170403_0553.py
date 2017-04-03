bfb0afe7f8dab2b0bfb16f8a09d8fd634f047a70
tuerlefl <florian.tuerler@hotmail.com>
2017-04-03 16:31:33 +0200

    fix data migration categories

--- allink_core/allink_categories/migrations/0018_auto_20170403_0553.py
+++ allink_core/allink_categories/migrations/0018_auto_20170403_0553.py
@@ -9,23 +9,26 @@ def set_root_category_model_names(apps, schema_editor):
     from allink_core.allink_categories.models import AllinkCategory
     activate('de')
 
-    for root in AllinkCategory.get_root_nodes():
-        collected_model_names = []
-        for child in root.get_children():
-            for model_name in child.model_names:
-                # add the model_name if not already added
-                if model_name not in collected_model_names:
-                    collected_model_names.append(model_name)
+    if AllinkCategory.get_root_nodes():
+        for root in AllinkCategory.get_root_nodes():
+            collected_model_names = []
+            for child in root.get_children():
+                if child.model_names:
+                    for model_name in child.model_names:
+                        # add the model_name if not already added
+                        if model_name not in collected_model_names:
+                            collected_model_names.append(model_name)
 
-        # now add to root.model_names but keep the names which are already there
-        to_add = root.model_names
-        if collected_model_names:
-            for model_name in collected_model_names:
-                # add the model_name if not already added
-                if model_name not in to_add:
-                    to_add.append(model_name)
-            root.model_names = to_add
-            root.save()
+            # now add to root.model_names but keep the names which are already there
+            to_add = root.model_names if root.model_names else []
+
+            if collected_model_names:
+                for model_name in collected_model_names:
+                    # add the model_name if not already added
+                    if model_name not in to_add:
+                        to_add.append(model_name)
+                root.model_names = to_add
+                root.save()
 
 
 
