e96916602af46e0134f5a0e166ead97a2a5c9f3d
tuerlefl <florian.tuerler@hotmail.com>
2017-04-04 17:51:11 +0200

    fix form djangocms_vid

--- allink_core/djangocms_vid/forms.py
+++ allink_core/djangocms_vid/forms.py
@@ -1,29 +1,38 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from allink_core.allink_base.utils import get_additional_choices
+from allink_core.djangocms_vid.models import AllinkVidFilePlugin, AllinkVidEmbedPlugin
 
-from allink_core.djangocms_vid.models import AllinkVidPlugin
 
-
-class AllinkVidPluginForm(forms.ModelForm):
+class AllinkVidEmbedPluginForm(forms.ModelForm):
 
     class Meta:
-        model = AllinkVidPlugin
+        model = AllinkVidEmbedPlugin
         exclude = ('page', 'position', 'placeholder', 'language', 'plugin_type')
-        widgets = {
-            'caption_text': forms.Textarea(attrs={'rows': 2}),
-        }
 
     def __init__(self, *args, **kwargs):
-        super(AllinkVidPluginForm, self).__init__(*args, **kwargs)
-        self.fields['template'] = forms.CharField(
-            label=_(u'Template'),
-            widget=forms.Select(choices=self.instance.get_templates()),
-            required=True,
-        )
-        self.fields['ratio'] = forms.CharField(
-            label=_(u'Ratio'),
-            help_text=_(u'This option overrides the default settings for the content plugin.'),
-            widget=forms.Select(choices=self.instance.get_ratio_choices()),
-            required=False,
-        )
+        super(AllinkVidEmbedPluginForm, self).__init__(*args, **kwargs)
+        if get_additional_choices('PROJECT_CSS_CLASSES_VIDEO'):
+            self.fields['project_css_classes'] = forms.MultipleChoiceField(
+                label=_(u'Predifined css classes for Video Plugin'),
+                help_text=_(u'Instructions: Single selection is made by clicking on an option. Multiple selections are achieved by pressing and holding down the Command-key (Mac) or Control-Key (Windows) <strong>and</strong> clicking the options you would like to apply.'),
+                choices=get_additional_choices('PROJECT_CSS_CLASSES_VIDEO'),
+                required=False,
+            )
+
+class AllinkVidFilePluginForm(forms.ModelForm):
+
+    class Meta:
+        model = AllinkVidFilePlugin
+        exclude = ('page', 'position', 'placeholder', 'language', 'plugin_type')
+
+    def __init__(self, *args, **kwargs):
+        super(AllinkVidFilePluginForm, self).__init__(*args, **kwargs)
+        if get_additional_choices('PROJECT_CSS_CLASSES_VIDEO'):
+            self.fields['project_css_classes'] = forms.MultipleChoiceField(
+                label=_(u'Predifined css classes for Video Plugin'),
+                help_text=_(u'Instructions: Single selection is made by clicking on an option. Multiple selections are achieved by pressing and holding down the Command-key (Mac) or Control-Key (Windows) <strong>and</strong> clicking the options you would like to apply.'),
+                choices=get_additional_choices('PROJECT_CSS_CLASSES_VIDEO'),
+                required=False,
+            )
