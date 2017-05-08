9f2f1cd51162a8374ccc3a865676f2fb64bcdfc0
tuerlefl <florian.tuerler@hotmail.com>
2017-05-08 11:34:08 +0200

    remove htmlmin

--- allink_core/allink_base/utils/middleware.py
+++ allink_core/allink_base/utils/middleware.py
@@ -3,21 +3,21 @@ import re
 from django.conf import settings
 from django.http import HttpResponsePermanentRedirect
 
-from htmlmin.middleware import HtmlMinifyMiddleware
-from htmlmin.minify import html_minify
+# from htmlmin.middleware import HtmlMinifyMiddleware
+# from htmlmin.minify import html_minify
 
 
-class AllinkHtmlMinifyMiddleware(HtmlMinifyMiddleware):
-
-    def process_response(self, request, response):
-        minify = getattr(settings, "HTML_MINIFY", not settings.DEBUG)
-        keep_comments = getattr(settings, 'KEEP_COMMENTS_ON_MINIFYING', False)
-        parser = getattr(settings, 'HTML_MIN_PARSER', 'html5lib')
-        # if minify and self.can_minify_response(request, response) and hasattr(response, 'rendered_content'):
-        #     response.content = html_minify(response.rendered_content,
-        #                                    ignore_comments=not keep_comments,
-        #                                    parser=parser)
-        return response
+# class AllinkHtmlMinifyMiddleware(HtmlMinifyMiddleware):
+#
+#     def process_response(self, request, response):
+#         minify = getattr(settings, "HTML_MINIFY", not settings.DEBUG)
+#         keep_comments = getattr(settings, 'KEEP_COMMENTS_ON_MINIFYING', False)
+#         parser = getattr(settings, 'HTML_MIN_PARSER', 'html5lib')
+#         # if minify and self.can_minify_response(request, response) and hasattr(response, 'rendered_content'):
+#         #     response.content = html_minify(response.rendered_content,
+#         #                                    ignore_comments=not keep_comments,
+#         #                                    parser=parser)
+#         return response
 
 
 class AllinkUrlRedirectMiddleware:
