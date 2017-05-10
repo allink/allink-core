265f891193386c0e8753713adc031a79a412b542
tuerlefl <florian.tuerler@hotmail.com>
2017-05-10 16:12:57 +0200

    translations po

--- allink_core/allink_config/allink_settings.py
+++ allink_core/allink_config/allink_settings.py
@@ -153,17 +153,61 @@ DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TOOLBAR_CALLBACK": show_toolbar,
 }
 
-# DEBUG_TOOLBAR_PANELS = [
-#     'debug_toolbar.panels.versions.VersionsPanel',
-#     'debug_toolbar.panels.timer.TimerPanel',
-#     'debug_toolbar.panels.settings.SettingsPanel',
-#     'debug_toolbar.panels.headers.HeadersPanel',
-#     'debug_toolbar.panels.request.RequestPanel',
-#     'debug_toolbar.panels.sql.SQLPanel',
-#     'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-#     'debug_toolbar.panels.templates.TemplatesPanel',
-#     'debug_toolbar.panels.cache.CachePanel',
-#     'debug_toolbar.panels.signals.SignalsPanel',
-#     'debug_toolbar.panels.logging.LoggingPanel',
-#     'debug_toolbar.panels.redirects.RedirectsPanel',
-# ]
+
+####################################################################################
+
+# =Thumbnail width aliases
+
+THUMBNAIL_WIDTH_ALIASES = {
+    '1-of-1': {
+        'xs': {'width': 450, 'ratio': '3-2'},
+        'sm': {'width': 1200, 'ratio': '3-2'},
+        'xl': {'width': 1500, 'ratio': '3-2'}
+    },
+    '1-of-2': {
+        'xs': {'width': 450, 'ratio': '3-2'},
+        'sm': {'width': 1200, 'ratio': '3-2'},
+        'xl': {'width': 1500, 'ratio': '3-2'}
+    },
+    '2-of-3': {
+        'xs': {'width': 450, 'ratio': '3-2'},
+        'sm': {'width': 1200, 'ratio': '3-2'},
+        'xl': {'width': 1500, 'ratio': '3-2'}
+    },
+    '1-of-3': {
+        'xs': {'width': 450, 'ratio': '3-2'},
+        'sm': {'width': 1200, 'ratio': '3-2'},
+        'xl': {'width': 1500, 'ratio': '3-2'}
+    },
+    '1-of-4': {
+        'xs': {'width': 450, 'ratio': '3-2'},
+        'sm': {'width': 1200, 'ratio': '3-2'},
+        'xl': {'width': 1500, 'ratio': '3-2'}
+    },
+    '1-of-5': {
+        'xs': {'width': 450, 'ratio': '3-2'},
+        'sm': {'width': 1200, 'ratio': '3-2'},
+        'xl': {'width': 1500, 'ratio': '3-2'}
+    },
+    '1-of-6': {
+        'xs': {'width': 450, 'ratio': '3-2'},
+        'sm': {'width': 1200, 'ratio': '3-2'},
+        'xl': {'width': 1500, 'ratio': '3-2'}
+    }
+}
+
+# 'fullscreen', {
+#         'xs': {'width': 450, 'ratio': '3-2},
+#         'sm': {'width': 1200, 'ratio': '3-2},
+#         'xl': {'width': 1500, 'ratio': '3-2},
+#     },\
+# 'container', {
+#         'xs': {'width': 450, 'ratio': '3-2},
+#         'sm': {'width': 1200, 'ratio': '3-2},
+#         'xl': {'width': 1500, 'ratio': '3-2},
+#     },
+# 'parallax', {
+#         'xs': {'width': 450, 'ratio': '3-2},
+#         'sm': {'width': 1200, 'ratio': '3-2},
+#         'xl': {'width': 1500, 'ratio': '3-2},
+#     },
