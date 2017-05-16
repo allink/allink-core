e850c2d3c6096a0aa0135456ab927b1b9411a53e
leandrafinger <leandra.finger@gmail.com>
2017-05-16 09:35:30 +0200

    link field in progress

--- allink_core/allink_legacy_redirect/forms.py
+++ allink_core/allink_legacy_redirect/forms.py
@@ -4,6 +4,7 @@ from django import forms
 from allink_core.allink_legacy_redirect.models import AllinkLegacyLink
 from allink_core.allink_base.models.model_fields import choices_from_sitemaps
 from allink_core.allink_base.forms.fields import SelectLinkField
+from allink_core.allink_base.forms.widgets import SearchSelectWidget
 
 
 class AllinkLegacyChangeAdminForm(forms.ModelForm):
@@ -18,3 +19,13 @@ class AllinkLegacyChangeAdminForm(forms.ModelForm):
         super(AllinkLegacyChangeAdminForm, self).__init__(*args, **kwargs)
         self.fields['new_page'].choices = choices_from_sitemaps()
         self.fields['additional'].choices = self.fields['additional'].get_page_and_app_choices()
+
+    def save(self, *args, **kwargs):
+        # json.dumps({'page_id': p.id}
+        # json.dumps({'link_apphook_page_id': p.id, 'link_url_name': url_name, 'link_object_id': obj.id, 'link_url_kwargs': info[1]}
+        for field_name, field in filter(lambda x: isinstance(x[1].widget, SearchSelectWidget), self.fields.items()):
+            self.save_search_select_field()
+        super(AllinkLegacyChangeAdminForm, self).save(*args, **kwargs)
+
+    def save_search_select_field(self):
+        pass
