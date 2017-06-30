f652e6d8fbd9b2144f63cc8b1537e74e70db0fba
tuerlefl <florian.tuerler@hotmail.com>
2017-06-30 19:21:47 +0200

    refactor seo tag

--- allink_core/allink_base/templatetags/allink_seo_tags.py
+++ allink_core/allink_base/templatetags/allink_seo_tags.py
@@ -10,6 +10,71 @@ register = template.Library()
 ####################################################################################
 # Meta and og: > renders all meta and og:tags
 
+def get_page_title(allink_config, disable_base, title_prio_1, title_prio_2, title_prio_3, base_prio_1, base_prio_2):
+    page_title_full = ''
+    # base title
+    if disable_base:
+        base = ''
+    else:
+        if base_prio_1:
+            base = ' | ' + base_prio_1
+        elif getattr(allink_config, 'default_base_title'):
+            base = ' | ' + getattr(allink_config, 'default_base_title', '')
+        else:
+            base = ' | ' + base_prio_2
+
+    # page title (<title>)
+    if title_prio_1:
+        page_title_full = page_title_full + base
+    elif title_prio_2:
+        page_title_full = title_prio_2 + base
+    elif title_prio_3:
+        page_title_full = title_prio_3 + base
+    else:
+        page_title_full = getattr(allink_config, 'default_base_title', '')
+
+    return page_title_full
+
+
+def get_og_title(allink_config, prio_1, prio_2, prio_3):
+    # title (for og:title)
+    if prio_1:
+        og_title = prio_1
+    elif prio_2:
+        og_title = prio_2
+    elif prio_3:
+        og_title = prio_3
+    else:
+        og_title = getattr(allink_config, 'default_base_title', '')
+    return og_title
+
+
+def get_description(prio_1, prio_2, prio_3):
+    if prio_1:
+        description = prio_1
+    elif prio_2:
+        description = prio_2
+    elif prio_3:
+        description = prio_3
+    else:
+        description = ''
+    return description
+
+
+def get_image_url(allink_config, prio_1, prio_2, prio_3):
+    if prio_1:
+        image_url = prio_1.url
+    elif prio_2:
+        image_url = prio_2.url
+    elif prio_3:
+        image_url = prio_3.url
+    else:
+        try:
+            image_url = allink_config.default_og_image.url
+        except AttributeError:
+            image_url = ''
+    return image_url
+
 
 @register.inclusion_tag('templatetags/allink_meta_og.html', takes_context=True)
 def render_meta_og(context, obj=None, page_title=None, base_page_title=None, image=None, og_title=None, description=None):
@@ -25,119 +90,53 @@ def render_meta_og(context, obj=None, page_title=None, base_page_title=None, ima
     #  we pass a object when app content
     if obj:
 
-        # base title
-        if obj and obj.disable_base_title:
-            base = ''
-        else:
-            if base_page_title:
-                base = ' | ' + base_page_title
-            elif getattr(allink_config, 'default_base_title', ''):
-                base = ' | ' + getattr(allink_config, 'default_base_title', '')
-            else:
-                base = ' | ' + site.name
+        page_title = get_page_title(
+            allink_config,
+            obj.disable_base_title,
+            page_title,
+            obj.og_title,
+            obj.title,
+            base_page_title,
+            site.name
+        )
 
-        # page title (<title>)
-        if page_title:
-            page_title += base
-        elif obj.og_title:
-            page_title = obj.og_title + base
-        elif obj.title:
-            page_title = obj.title + base
-        else:
-            page_title = getattr(allink_config, 'default_base_title', '')
-
-        # title (for og:title)
-        if og_title:
-            og_title = og_title
-        elif obj.og_title:
-            og_title = obj.og_title
-        elif obj.title:
-            og_title = obj.title
-        else:
-            og_title = getattr(allink_config, 'default_base_title', '')
-
-        # description
-        if description:
-            description = description
-        elif obj.og_description:
-            description = obj.og_description
-        elif obj.lead:
-            description = obj.lead
-        else:
-            description = ''
-
-        # image
-        if image:
-            image_url = image.url
-        elif obj.og_image:
-            image_url = obj.og_image.url
-        elif obj.preview_image:
-            image_url = obj.preview_image.url
-        else:
-            try:
-                image_url = allink_config.default_og_image.url
-            except:
-                image_url = None
+        og_title = get_og_title(allink_config, og_title, obj.og_title, obj.title)
+        description = get_description(description, obj.og_description, obj.lead)
+        image_url = get_image_url(allink_config, image, getattr(obj, 'og_image'), getattr(obj, 'preview_image'))
 
     # cms page (no object is supplied)
     elif hasattr(context.request, 'current_page'):
         page = getattr(context.request, 'current_page')
+        # is it a cms page (or is it a non)
+        if page:
+            cms_page_title = page.get_page_title()
+            cms_meta_descr = page.get_meta_description()
+        else:
+            cms_page_title = None
+            cms_meta_descr = None
+
         try:
-            # only when extension is there
+            # only when extension is there (cms page must not have a extension)
             page_ext = page.get_title_obj().allinkmetatagextension
+            page_ext_og_image = getattr(page_ext, 'og_image')
         except:
             page_ext = None
+            page_ext_og_image = None
 
-        # base title
-        if page_ext and page_ext.disable_base_title:
-            base = ''
-        else:
-            if base_page_title:
-                base = ' | ' + base_page_title
-            elif getattr(allink_config, 'default_base_title', ''):
-                base = ' | ' + getattr(allink_config, 'default_base_title', '')
-            else:
-                base = ' | ' + site.name
+        page_title = get_page_title(
+            allink_config,
+            getattr(page_ext, 'disable_base_title', False),
+            page_title,
+            cms_page_title,
+            None,
+            base_page_title,
+            site.name
+        )
 
+        og_title = get_og_title(allink_config, og_title, cms_page_title, None)
+        description = get_description(description, cms_meta_descr, None)
+        image_url = get_image_url(allink_config, image, page_ext_og_image, None)
 
-        # page title (<title>)
-        if page_title:
-            page_title += base
-        elif page and page.get_page_title():
-            page_title = page.get_page_title() + base
-        else:
-            page_title = getattr(allink_config, 'default_base_title', '')
-
-        # og:title
-        if og_title:
-            og_title = og_title
-        elif page and page.get_page_title():
-            og_title = page.get_page_title()
-        else:
-            og_title = getattr(allink_config, 'default_base_title', '')
-
-        # description
-        if description:
-            description = description
-        elif page:
-            description = page.get_meta_description()
-        else:
-            # for example django admin page
-            description = ''
-
-        # image
-        if image:
-            image_url = image.url
-        elif page_ext and page_ext.og_image:
-            try:
-                image_url = page_ext.og_image.url if page_ext.og_image else allink_config.default_og_image.url
-            except:
-                image_url = None
-        else:
-            try:
-                image_url = allink_config.default_og_image.url
-            except:
-                image_url = None
 
     additional_context = {
         'page_title': page_title,
