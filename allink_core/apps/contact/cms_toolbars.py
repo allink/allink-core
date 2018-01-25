7a28b0e67b0e8b656c52c035b0902275d82d89a7
tuerlefl <florian.tuerler@hotmail.com>
2018-01-25 18:01:39 +0100

    toolbar check for permissions events and contact

--- allink_core/apps/contact/cms_toolbars.py
+++ allink_core/apps/contact/cms_toolbars.py
@@ -18,11 +18,10 @@ class ContactToolbar(CMSToolbar):
 
     def populate(self):
         opts = self.model._meta
-        menu = self.toolbar.get_or_create_menu('form-menu', _('Forms'))
 
         if self.request.user.has_perm('%s.%s' % (opts.app_label, get_permission_codename('change', opts))):
+            menu = self.toolbar.get_or_create_menu('form-menu', _('Forms'))
             url = reverse('admin:{}_{}_changelist'.format(self.model._meta.app_label, self.model._meta.model_name))
             menu.add_sideframe_item(self.model.get_verbose_name_plural(), url=url)
-
 
 toolbar_pool.register(ContactToolbar)
