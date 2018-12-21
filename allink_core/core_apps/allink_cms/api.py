8dbdb866e231aadb4c6f9236bea095c90073432e
tuerlefl <florian.tuerler@hotmail.com>
2018-12-21 12:39:24 +0100

    pass the original request to CMSPluginAPIView

--- allink_core/core_apps/allink_cms/api.py
+++ allink_core/core_apps/allink_cms/api.py
@@ -5,12 +5,14 @@ from rest_framework.permissions import AllowAny
 
 from rest_framework.renderers import TemplateHTMLRenderer
 from django.http import HttpResponse
+from cms.models.pagemodel import Page
 
 
 class CMSPluginAPIView(RetrieveAPIView):
     """
     - this will return the rendered html for a specific plugin
     - it will not render child plugins
+    - some plugins need the current_page, so you can pass an id as a GET param
     """
     permission_classes = (AllowAny, )
     lookup_field = 'id'
@@ -27,6 +29,14 @@ class CMSPluginAPIView(RetrieveAPIView):
 
         renderer = ContentRenderer(request)
         context = RequestContext(request)
+
+        try:
+            # if current_page is supplied, we care about the original request
+            request.current_page = Page.objects.get(id=request.GET.get('current_page'))
+            request.path_info = request.current_page.get_absolute_url(language=request.LANGUAGE_CODE)
+        except (KeyError, Page.DoesNotExist):
+            pass
+
         # Avoid errors if plugin require a request object
         # when rendering.
         context['request'] = request
