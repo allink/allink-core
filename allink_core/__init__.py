96c3bae9d8decda9d1dc21032fc0c1bc4bfd5dde
Marius Küng <maku@mailbox.org>
2019-10-14 17:24:55 +0200

    Bump version: 2.0.5 → 2.0.6

--- allink_core/__init__.py
+++ allink_core/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '2.0.5'
+__version__ = '2.0.6'
 
 # Cheeky setting that allows each template to be accessible by two paths.
 # Eg: the template 'allink/templates/allink/base.html' can be accessed via both
