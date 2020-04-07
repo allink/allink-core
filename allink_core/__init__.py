07d1a67941c86b14e67009f97d3807678a677380
chal <haldner@allink.ch>
2020-04-07 11:00:29 +0200

    Bump version: 2.4.0 → 2.5.0

--- allink_core/__init__.py
+++ allink_core/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '2.4.0'
+__version__ = '2.5.0'
 
 # Cheeky setting that allows each template to be accessible by two paths.
 # Eg: the template 'allink/templates/allink/base.html' can be accessed via both
