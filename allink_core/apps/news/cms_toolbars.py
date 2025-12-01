ad4a11ef3bd27af6946691ba4670c927a7422f8f
Leon Osenger <111272821+LeonOsenger@users.noreply.github.com>
2025-12-01 15:12:53 +0100

    add knowledge apphook to toolbar

--- allink_core/apps/news/cms_toolbars.py
+++ allink_core/apps/news/cms_toolbars.py
@@ -6,6 +6,7 @@ from allink_core.core.cms_toolbars import AllinkBaseModifierMixin
 from allink_core.core.loading import get_model
 
 News = get_model('news', 'News')
+Knowledge = get_model('news', 'Knowledge')
 
 
 class NewsToolbar(AllinkBaseModifierMixin, CMSToolbar):
@@ -13,4 +14,11 @@ class NewsToolbar(AllinkBaseModifierMixin, CMSToolbar):
     app_label = News._meta.app_label
 
 
+
+class KnowledgeToolbar(AllinkBaseModifierMixin, CMSToolbar):
+    model = Knowledge
+    app_label = Knowledge._meta.app_label
+
+
 toolbar_pool.register(NewsToolbar)
+toolbar_pool.register(KnowledgeToolbar)
